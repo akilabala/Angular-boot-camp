1fa1b565698fc2cb2d0cccc69bd6f7e5d8bda585
Anoop Narayanan <anarayanan@prosper.com>
2015-11-03 14:01:17 -0800

    Unit Test cases

--- src/app/user-list/user-list.spec.js
+++ src/app/user-list/user-list.spec.js
@@ -1,3 +1,54 @@
-/**
- * Created by anarayanan on 11/3/15.
- */
+describe('userListCtrl', function () {
+
+  var userListCtrl,
+    userListModel;
+
+  beforeEach(module('ps.user-list'));
+
+
+  //This is one way to mock it.
+
+  /*beforeEach(inject(function ($controller) {
+    userListCtrl = $controller('UserListCtrl', {
+      userListModel: {
+        getUsers: function () {
+          return {
+            then: function () {
+            }
+          }
+        }
+      }
+    });
+  }));*/
+
+
+  beforeEach(inject(function ($controller) {
+    userListModel = {
+      getUsersWasCalled: false,
+      getUsers: function () {
+        this.getUsersWasCalled = true;
+        return {
+          then: function (callback) {
+            callback([]);
+          }
+        }
+      }
+    };
+    userListCtrl = $controller('UserListCtrl', {
+      userListModel: userListModel
+    });
+  }));
+
+  it('should create an instance', function () {
+    expect(userListCtrl).toBeDefined();
+  });
+
+  it('should call getUsers on userListModel when instantiated', function () {
+    expect(userListModel.getUsersWasCalled).toBeTruthy();
+  });
+
+  it('should set the users property when getUsers returns a value', function () {
+    expect(userListCtrl.users).toBeDefined();
+  });
+
+});
