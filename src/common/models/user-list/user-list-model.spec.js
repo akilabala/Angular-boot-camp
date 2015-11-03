4f85187bbb09a3ed9311fbac7a246c1a75c4f1f8
Anoop Narayanan <anarayanan@prosper.com>
2015-11-03 14:27:22 -0800

    Unit Test cases

--- src/common/models/user-list/user-list-model.spec.js
+++ src/common/models/user-list/user-list-model.spec.js
@@ -1,3 +1,40 @@
-/**
- * Created by anarayanan on 11/3/15.
- */
+describe('userListModel', function () {
+
+  var userListModel,
+    $httpBackend,
+    allUsers;
+
+  beforeEach(module('ps.models.user-list'));
+
+  beforeEach(inject(function (_userListModel_, _$httpBackend_) {
+    userListModel = _userListModel_;
+    $httpBackend = _$httpBackend_;
+    setUpMocks();
+  }));
+
+
+  it('should create an instance', function () {
+    expect(userListModel).toBeDefined();
+  });
+
+  describe('getUsers', function () {
+
+    it('should fetch users when called', function () {
+      userListModel.getUsers().then(function(users){
+        expect(users).toBeDefined();
+        expect(users.length).toBeGreaterThan(0);
+        expect(users).toEqual(allUsers);
+      });
+      $httpBackend.flush();
+    });
+  });
+
+
+  function setUpMocks(){
+    jasmine.getJSONFixtures().fixturesPath = 'base/data/users/';
+    allUsers = getJSONFixture('default.json');
+
+    $httpBackend.whenGET('http://api.github.com/users').respond(allUsers);
+  }
+
+});
